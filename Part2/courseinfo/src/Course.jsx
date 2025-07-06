1e2029f20d449843f2b5cb202f23aa040ef7e9c3
Imamasharmeen <imamasharmeen@gmail.com>
2025-07-06 10:01:24 +0300

    Course information step 7

--- Part2/courseinfo/src/Course.jsx
+++ Part2/courseinfo/src/Course.jsx
@@ -1,34 +1,30 @@
-import React from 'react'
+const Header = ({ name }) => <h1>{name}</h1>
 
-const Header = ({ name }) => {
-  return <h1>{name}</h1>
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+)
+
+const Content = ({ parts }) => (
+  <div>
+    {parts.map(part => (
+      <Part key={part.id} part={part} />
+    ))}
+  </div>
+)
+
+const Total = ({ parts }) => {
+  const total = parts.reduce((sum, part) => sum + part.exercises, 0)
+  return <p><strong>total of {total} exercises</strong></p>
 }
 
-const Part = ({ part }) => {
-  return (
-    <p>
-      {part.name} {part.exercises}
-    </p>
-  )
-}
-
-const Content = ({ parts }) => {
-  return (
-    <div>
-      {parts.map(part => (
-        <Part key={part.id} part={part} />
-      ))}
-    </div>
-  )
-}
-
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header name={course.name} />
-      <Content parts={course.parts} />
-    </div>
-  )
-}
+const Course = ({ course }) => (
+  <div>
+    <Header name={course.name} />
+    <Content parts={course.parts} />
+    <Total parts={course.parts} />
+  </div>
+)
 
 export default Course
