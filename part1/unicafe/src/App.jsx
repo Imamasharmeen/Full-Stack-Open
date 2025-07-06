c45eda2189bc9e0d569134975beb866e82c09236
Imamasharmeen <imamasharmeen@gmail.com>
2025-07-06 07:25:22 +0300

    unicafe step 6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,9 +5,12 @@ const Button = ({ onClick, text }) => (
   <button onClick={onClick}>{text}</button>
 )
 
-// StatisticLine Component
+// StatisticLine Component (Table row)
 const StatisticLine = ({ text, value }) => (
-  <p>{text} {value}</p>
+  <tr>
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
 )
 
 // Statistics Component
@@ -29,12 +32,16 @@ const Statistics = ({ good, neutral, bad }) => {
   return (
     <div>
       <h2>statistics</h2>
-      <StatisticLine text="good" value={good} />
-      <StatisticLine text="neutral" value={neutral} />
-      <StatisticLine text="bad" value={bad} />
-      <StatisticLine text="all" value={total} />
-      <StatisticLine text="average" value={average.toFixed(1)} />
-      <StatisticLine text="positive" value={`${positive.toFixed(1)} %`} />
+      <table>
+        <tbody>
+          <StatisticLine text="good" value={good} />
+          <StatisticLine text="neutral" value={neutral} />
+          <StatisticLine text="bad" value={bad} />
+          <StatisticLine text="all" value={total} />
+          <StatisticLine text="average" value={average.toFixed(1)} />
+          <StatisticLine text="positive" value={`${positive.toFixed(1)} %`} />
+        </tbody>
+      </table>
     </div>
   )
 }
