05de6f848270deaaabab72b1dc1936f654196fef
Imamasharmeen <imamasharmeen@gmail.com>
2025-09-29 13:00:12 +0300

    Fix weather image.

--- Part2/countries/src/App.jsx
+++ Part2/countries/src/App.jsx
@@ -91,7 +91,7 @@ const App = () => {
           <div>
             <p>Temperature: {weather.main.temp} °C</p>
             <img
-              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}.png`}
               alt={weather.weather[0].description}
             />
             <p>Wind: {weather.wind.speed} m/s</p>
