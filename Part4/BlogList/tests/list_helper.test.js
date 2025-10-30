cfab200aadee7a52f20286bc5777fef491d9e72a
Imamasharmeen <imamasharmeen@gmail.com>
2025-10-30 09:43:46 +0200

    4.4: Helper Functions and Unit Tests, step 2

--- Part4/BlogList/tests/list_helper.test.js
+++ Part4/BlogList/tests/list_helper.test.js
@@ -1,12 +1,48 @@
-// tests/list_helper.test.js
-
-const { test } = require('node:test')
+const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../utils/list_helper')
 
+
 test('dummy returns one', () => {
   const blogs = []
-
   const result = listHelper.dummy(blogs)
   assert.strictEqual(result, 1)
+})
+
+
+describe('total likes', () => {
+  
+  const listWithOneBlog = [
+    {
+      _id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+      __v: 0,
+    },
+  ]
+
+  // new line added: corresponding test case
+  test('when list has only one blog, equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    assert.strictEqual(result, 5)
+  })
+
+  // new line added: 
+  test('when list has multiple blogs, equals the sum of likes', () => {
+    const blogs = [
+      { title: 'Blog 1', author: 'A', likes: 3 },
+      { title: 'Blog 2', author: 'B', likes: 5 },
+      { title: 'Blog 3', author: 'C', likes: 2 },
+    ]
+    const result = listHelper.totalLikes(blogs)
+    assert.strictEqual(result, 10)
+  })
+
+  // new line added: 
+  test('of empty list is zero', () => {
+    const result = listHelper.totalLikes([])
+    assert.strictEqual(result, 0)
+  })
 })
--- Part4/BlogList/utils/list_helper.js
+++ Part4/BlogList/utils/list_helper.js
@@ -1,10 +1,25 @@
-// utils/list_helper.js
 
-// dummy: receives an array of blogs and always returns 1
 const dummy = (blogs) => {
   return 1
 }
 
+//  new line added: totalLikes function define 
+const totalLikes = (blogs) => {
+
+  if (blogs.length === 0) {
+    return 0
+  }
+
+  
+  const total = blogs.reduce((sum, blog) => {
+    return sum + blog.likes
+  }, 0)
+
+  return total
+}
+
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes, 
 }
