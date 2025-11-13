fe470e3683769131cf7eafb1e89c082225b7c946
Imamasharmeen <imamasharmeen@gmail.com>
2025-11-13 13:30:23 +0200

    7.9: Automatic Code Formatting

--- Part7/routed-anecdotes/.eslintrc.cjs
+++ Part7/routed-anecdotes/.eslintrc.cjs
@@ -1,20 +1,24 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended'   // <-- integrates Prettier with ESLint
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  settings: { react: { version: '18.2' } },
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react/react-in-jsx-scope': 'off',
+    'prettier/prettier': 'error',   // <-- Prettier errors show in ESLint
   },
 }
--- Part7/routed-anecdotes/src/components/AnecdoteList.jsx
+++ Part7/routed-anecdotes/src/components/AnecdoteList.jsx
@@ -5,7 +5,7 @@ const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
     <ul>
-      {anecdotes.map(a =>
+      {anecdotes.map((a) => (
         <li key={a.id}>
           <Link to={`/anecdotes/${a.id}`}>{a.content}</Link>
         </li>
