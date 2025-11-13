3fd7181961dc43c0efd221a1e54e103805c62c9a
Imamasharmeen <imamasharmeen@gmail.com>
2025-11-13 02:31:38 +0200

    6.11 Anecdotes, step 9

--- Part6/unicafe-redux/src/reducers/anecdoteReducer.js
+++ Part6/unicafe-redux/src/reducers/anecdoteReducer.js
@@ -1,60 +1,39 @@
-// src/reducers/anecdoteReducer.js
+import { createSlice } from '@reduxjs/toolkit'
 
-const generateId = () => Number((Math.random() * 1000000).toFixed(0))
+const getId = () => (100000 * Math.random()).toFixed(0)
 
-const initialAnecdotes = [
-  'If it hurts, do it more often',
-  'Adding manpower to a late software project makes it later!',
-  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-  'Premature optimization is the root of all evil.',
-  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
-]
-
-const asObject = content => {
-  return {
-    content,
-    id: generateId(),
+const initialState = [
+  {
+    content: 'If it hurts, do it more often',
+    id: getId(),
+    votes: 0
+  },
+  {
+    content: 'Adding manpower to a late software project makes it later!',
+    id: getId(),
     votes: 0
   }
-}
+]
 
-const initialState = initialAnecdotes.map(asObject)
-
-export const voteAnecdote = (id) => {
-  return {
-    type: 'VOTE',
-    payload: { id }
-  }
-}
-
-export const createAnecdote = (content) => {
-  return {
-    type: 'NEW_ANECDOTE',
-    payload: {
-      content,
-      id: generateId(),
-      votes: 0
+const anecdoteSlice = createSlice({
+  name: 'anecdotes',
+  initialState,
+  reducers: {
+    voteAnecdote(state, action) {
+      const id = action.payload
+      return state.map(a =>
+        a.id !== id ? a : { ...a, votes: a.votes + 1 }
+      )
+    },
+    addAnecdote(state, action) {
+      state.push({
+        content: action.payload,
+        id: getId(),
+        votes: 0
+      })
     }
   }
-}
+})
 
-const anecdoteReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case 'VOTE': {
-      const id = action.payload.id
-      const anecdote = state.find(a => a.id === id)
-      const changed = { ...anecdote, votes: anecdote.votes + 1 }
-      return state
-        .map(a => a.id !== id ? a : changed)
-        .sort((a, b) => b.votes - a.votes)
-    }
-
-    case 'NEW_ANECDOTE':
-      return [...state, action.payload]
-
-    default:
-      return state
-  }
-}
-
-export default anecdoteReducer
+export const { voteAnecdote, addAnecdote } = anecdoteSlice.actions
+export default anecdoteSlice.reducer
