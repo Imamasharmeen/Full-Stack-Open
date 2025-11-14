0ccf714750d2648749b3dc65311534e0c59f49c4
Imamasharmeen <imamasharmeen@gmail.com>
2025-11-14 23:59:27 +0200

    4.7 compleated

--- Part_7/routed-anecdotes/src/components/CreateNew.jsx
+++ Part_7/routed-anecdotes/src/components/CreateNew.jsx
@@ -1,33 +1,33 @@
 /* eslint-disable react/prop-types */
-import { useNavigate } from 'react-router-dom'
-import { useFiled } from '../hooks'
+import { useNavigate } from "react-router-dom";
+import { useFiled } from "../hooks";
 
 const CreateNew = (props) => {
-  const content = useFiled('text')
-  const author = useFiled('text')
-  const info = useFiled('text')
+  const content = useFiled("text");
+  const author = useFiled("text");
+  const info = useFiled("text");
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handleSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     props.addNew({
       content: content.field.value,
       author: author.field.value,
       info: info.field.value,
       votes: 0,
-    })
-    navigate('/')
-    props.showNotification(`a new anecdote ${content.value} created`)
-  }
+    });
+    navigate("/");
+    props.showNotification(`a new anecdote ${content.value} created`);
+  };
 
   const resetFields = (event) => {
-    event.preventDefault()
+    event.preventDefault();
 
-    content.reset()
-    author.reset()
-    info.reset()
-  }
+    content.reset();
+    author.reset();
+    info.reset();
+  };
 
   return (
     <div>
@@ -45,11 +45,11 @@ const CreateNew = (props) => {
           url for more info
           <input {...info.field} />
         </div>
-        <button type='submit'>create</button>
+        <button type="submit">create</button>
         <button onClick={resetFields}>reset</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default CreateNew
+export default CreateNew;
