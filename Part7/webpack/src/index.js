14dc64d3d7ce1ee23f947b764e46e3b81ed732c5
Sharmeen Akter Imama <100438343+Imamasharmeen@users.noreply.github.com>
2025-11-15 00:36:49 +0200

    Part7 directory

--- Part7/webpack/src/index.js
+++
@@ -1,8 +0,0 @@
-import 'core-js/stable/index.js'
-import 'regenerator-runtime/runtime.js'
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import App from './App'
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
